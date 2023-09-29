b4a58f7a6a40d01c6b834faafb8ef7c8c7e5dbe7
Lucas Araújo <lucasclash856@gmail.com>
2023-09-29 11:16:24 +0100

    OlaMundo

--- Semana01/OlaMundo/Program.cs
+++ Semana01/OlaMundo/Program.cs
@@ -2,10 +2,18 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// Esse programa imprime a mensagem "OlaMundo" na consola. 
+    /// </summary>   
     class Program
     {
+        /// <summary>
+        /// Este metodo é responsavel por imprimir a mensagem
+        /// </summary>
+        /// <param name="args"> Recebe argumentos da linha de comandos.</param>
         static void Main(string[] args)
         {
+            // imprime a mensagem 
             Console.WriteLine("Hello World!");
         }
     }
