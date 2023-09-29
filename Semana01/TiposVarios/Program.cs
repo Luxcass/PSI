eb6d565e3ea62a3a544c5ed8028bfb34a59c7f83
Lucas Araújo <lucasclash856@gmail.com>
2023-09-29 12:30:13 +0100

    bool

--- Semana01/TiposVarios/Program.cs
+++ Semana01/TiposVarios/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Text;
 
 namespace TiposVarios
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            Console.OutputEncoding = Encoding.UTF8; 
+            int numero = 7;
+            uint numero1 = 14U;
+            ulong numero2 = 21UL;
+            long numero3 = 28L;
+
+            float jett = 3.144f;
+            double reyna = 1.444;
+            decimal cypher = 5.233m;
+            
+            char skye = '\u03DF' ;
+            char sova = '\u5351' ;
+            bool kayo = true;
+            bool omen = false;
+
+
+            Console.WriteLine(omen);
+            Console.WriteLine(kayo);
+            Console.WriteLine(sova);
+            Console.WriteLine(skye);
+            Console.WriteLine(jett);
+            Console.WriteLine(reyna);
+            Console.WriteLine(cypher);
+            Console.WriteLine(numero);
+            Console.WriteLine(numero1);
+            Console.WriteLine(numero2);
+            Console.WriteLine(numero3);
+                        
         }
     }
 }
